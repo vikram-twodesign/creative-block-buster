7b2235332207f599e1042cc63d8c9486a62263c3
vikram-twodesign <131342697+vikram-twodesign@users.noreply.github.com>
2023-05-08 11:18:39 +0530

    contd.

--- api/submitForm.js
+++ api/submitForm.js
@@ -1,16 +1,34 @@
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
-  const { text } = req.query;
-  const formId = "1FAIpQLScvPGsfHcU1m4bF8zE7fa2KTElEs1trtC5XVjZIYuZYXkcY3g";
-  const entryId = "504692446";
-  const url = `https://docs.google.com/forms/d/e/${formId}/formResponse?usp=pp_url&entry.${entryId}=${encodeURIComponent(text)}`;
-
   try {
-    const response = await fetch(url, { method: "POST", mode: "no-cors" });
-    res.status(200).json({ message: "Submission successful" });
+    const text = req.query.text;
+
+    if (!text) {
+      res.status(400).send("Missing 'text' query parameter");
+      return;
+    }
+
+    const formId = "1FAIpQLScvPGsfHcU1m4bF8zE7fa2KTElEs1trtC5XVjZIYuZYXkcY3g";
+    const entryId = "504692446";
+    const url = `https://docs.google.com/forms/d/e/${formId}/formResponse?usp=pp_url&entry.${entryId}=${encodeURIComponent(text)}`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+    });
+
+    console.log("Response status:", response.status);
+    console.log("Response status text:", response.statusText);
+
+    if (!response.ok) {
+      res.status(response.status).send(response.statusText);
+      return;
+    }
+
+    res.status(200).send("Submission successful");
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ message: "Submission failed" });
+    res.status(500).send(`Error: ${error.message}`);
   }
 };
